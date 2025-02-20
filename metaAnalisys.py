d096fd4df8fea4eec16cefb7f4b4a60895f3837d
AntonioCherry <antonio.ceruso9988@gmail.com>
2025-02-20 11:57:51 +0100

    UPDATE gestione dati mancanti
    Imputazione dei dati mancanti nelle colonne Price EUR e Price USD tramite media
    Imputazione dati mancanti colonna Mana Cost per moda

--- metaAnalisys.py
+++ metaAnalisys.py
@@ -1,7 +1,32 @@
 import pandas as pd
 
 df = pd.read_csv("dataset.csv", sep=",")  # Usa sep=";" se i dati non si separano beneù
-# Mostra tutte le colonne senza troncamenti
 pd.set_option('display.max_columns', None)
+
+# Rimozione colonna Card Text
+df.drop(columns=['Card Text'], inplace=True)
+
+# Visualizza le prime righe del dataset
 print(df.head().to_string(index=False))
 
+# Selezionare solo le colonne numeriche
+colonne_numeriche = df.select_dtypes(include=['float64', 'int64']).columns
+
+# Imputare i valori mancanti con la media per ogni colonna numerica
+df[colonne_numeriche] = df[colonne_numeriche].apply(lambda x: x.fillna(x.mean()), axis=0)
+
+# Selezionare solo le colonne non numeriche (di tipo 'object' o 'category')
+colonne_non_numeriche = df.select_dtypes(include=['object', 'category']).columns
+
+# Imputare i valori mancanti con la moda per ogni colonna non numerica
+df[colonne_non_numeriche] = df[colonne_non_numeriche].apply(lambda x: x.fillna(x.mode()[0]), axis=0)
+
+# Stampare il DataFrame per verificare l'imputazione
+print(df.head())
+
+# Identificare le colonne con valori mancanti
+colonne_con_mancanti = df.columns[df.isna().sum() > 0]
+
+# Stampare il nome delle colonne con valori mancanti
+print("Colonne con valori mancanti:")
+print(colonne_con_mancanti)
